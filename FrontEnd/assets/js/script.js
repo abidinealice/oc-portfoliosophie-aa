d0d55814d7012f68a2584f9405ef20fa6a0729c2
Alice Abidine <abidinealice@gmail.com>
2023-12-02 17:19:42 +0100

    màj script index

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -3,24 +3,53 @@
 //API WORKS
 
 const urlWorks = "http://localhost:5678/api/works";
-const containerGallery = document.getElementById("gallery");
+const containerGallery = document.querySelector(".gallery");
 
-const getWorks = () => {
-  fetch(urlWorks)
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (works) {
-      console.log(works);
-      for (projects in works) {
-        containerGallery.innerHTML += `<figure>
-                <img src="${works[projects].imageUrl}" alt="${works[projects].title}">
-                <figcaption>${works[projects].title}</figcaption>
-        </figure>`;
-      }
-    });
-};
+async function getWorks() {
+  const response = await fetch(urlWorks);
+  const works = await response.json();
+  console.log(works);
+  console.log(typeof works);
+
+  const valuesWorks = JSON.stringify(works);
+  console.log(typeof valuesWorks);
+
+  window.localStorage.setItem("works", valuesWorks);
+  console.log(works);
+}
+
+getWorks();
+
+let work = window.localStorage.getItem("works");
+console.log(work);
+console.log(typeof work);
+let workProjects = JSON.parse(work);
+console.log(workProjects);
+console.log(typeof workProjects);
+
+// BOUTON FILTRES
+
+const buttonTous = document.getElementById("btnTous");
+const buttonObjets = document.getElementById("btnObjets");
+const buttonAppartements = document.getElementById("btnAppart");
+const buttonHotelres = document.getElementById("btnHotelres");
+
+buttonObjets.addEventListener("click", function onClick(event) {
+  containerGallery.innerHTML = "";
+
+  //async function getWork() {
+  //  const response = await fetch(urlWorks);
+  //  const works = await response.json();
+  //  return console.log(works);
+  //}
+
+  //let filteredCategories = getWork.filter((objects) => {
+  //  return objects.categoryID == 1;
+  //});
+  //getWork();
+});
 
 //LANCEMENT
 
-getWorks();
+//getWorks();
+//displayWorks();
