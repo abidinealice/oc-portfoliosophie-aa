ac50c8ee96fe909be5471079d702a903988f60e2
Alice Abidine <abidinealice@gmail.com>
2023-12-05 14:36:12 +0100

    login script

--- FrontEnd/assets/js/scriptLogin.js
+++ FrontEnd/assets/js/scriptLogin.js
@@ -4,7 +4,7 @@ const formLogin = document.querySelector(".form-login");
 //let formEmail = document.querySelector("#email");
 //let formPassword = document.querySelector("#password");
 const msgError = document.querySelector(".msg-error");
-console.log(msgError);
+//console.log(msgError);
 
 formLogin.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -15,13 +15,13 @@ formLogin.addEventListener("submit", function (event) {
     email: event.target.querySelector("[name=email]").value,
     password: event.target.querySelector("[name=password]").value,
   };
-  console.log(formData);
-  console.log(typeof formData);
+  //console.log(formData);
+  //console.log(typeof formData);
 
   //Creation de la charge utile au format JSON
   const chargeUtile = JSON.stringify(formData);
-  console.log(chargeUtile);
-  console.log(typeof chargeUtile);
+  //console.log(chargeUtile);
+  //console.log(typeof chargeUtile);
 
   // ON ENVOIE LES DONNES
   async function sendForm() {
@@ -34,22 +34,22 @@ formLogin.addEventListener("submit", function (event) {
       body: chargeUtile,
     });
     const valueToken = await response.json();
-    console.log(valueToken);
-    console.log(typeof valueToken);
+    //console.log(valueToken);
+    //console.log(typeof valueToken);
 
     if (response.ok) {
       const utilToken = JSON.stringify(valueToken);
       window.localStorage.setItem("valueToken", utilToken);
-      console.log(utilToken);
-      console.log(typeof utilToken);
+      //console.log(utilToken);
+      //console.log(typeof utilToken);
 
       //On cache le message d'erreur s'il est affiché
       if (msgError.hasAttribute("hidden") == false) {
         msgError.setAttribute("hidden", "");
       }
-      console.log(msgError);
+      //console.log(msgError);
 
-      //window.location.href = "./index.html";
+      window.location.href = "./index.html";
     } else {
       if (msgError.hasAttributes("hidden")) {
         msgError.removeAttribute("hidden");
